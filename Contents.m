da3b87ec872ab068cf8ef28d637d3037d8105249
Joseph H. Kennedy <jhkennedy@alaska.edu>
2009-07-09 12:19:36 -0800

    Updated contents file

--- Contents.m
+++ Contents.m
@@ -1,11 +1,18 @@
-% Code to recreate Figure 2 in Throstur Thorsteinsson's paper
+% Code to recreate the anisotrpy model in Throstur Thorsteinsson's paper
 %   An analytic approach to deformation of anisotropic ice-crystal
 %       aggregates
 %   Journal of Glaciology, Vol.47, No. 158, 2001 pg 507-516
 %
 % To run the model type 'THOR'
 %
+% Directory Contents:
 %
+% thor.m -- main program
 %
-% How I see this getting embeeded is: get stress tensor, then run the symbolics,
-% apply 
+% numb.m -- numeric method for recreating the figures in Thorsteinsson's
+% paper. This one is underestimating the integration. It maintains the
+% shape of the integral, however, it's 'amplitude' is too small when
+% comparing to the paper. 
+%
+% symb.m -- symolic method for recreating the figures in Thorsteinsson's
+% paper. This one is 'correct'.
